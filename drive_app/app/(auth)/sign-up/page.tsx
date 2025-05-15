8142efa6ccaea119876a759502e3922ca93c881d
Ink42 <lindomash001@gmail.com>
2025-05-15 02:26:43 +0200

    Update page.tsx

--- drive_app/app/(auth)/sign-up/page.tsx
+++ drive_app/app/(auth)/sign-up/page.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const SignUp = () => {
-  return <div>SignUp Page</div>;
-};
+
+const SignUp = () => <AuthForm type="sign-up" />;
 
 export default SignUp;
