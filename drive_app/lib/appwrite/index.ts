5c2b6bf1adb8142704003a2f93186e6844406c2b
SkylaarN <nkosinathisilima@gmail.com>
2025-05-18 16:50:22 +0100

    add: appwrite deps & config

---
+++ drive_app/lib/appwrite/index.ts
@@ -0,0 +1,46 @@
+import { appwriteConfig } from "@/lib/appwrite/config";
+import { Account, Client, Databases, Storage, Avatars } from "node-appwrite";
+import { cookies } from "next/headers";
+
+export const createSessionClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+
+  const session = (await cookies()).get("appwrite-session");
+
+  if (!session || !session.value) throw new Error("No session found");
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+  };
+};
+
+export const creatAdminClient = async () => {
+  const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secret);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get avatars() {
+      return new Avatars(client);
+    },
+  };
+};
